1bc51bb8c34690e7431c314d78d28337468e6048
Gareth Dawson <gwdawson.work@gmail.com>
2022-07-13 12:40:28 +0100

    feat: add data state and render components

---
+++ src/pages/homepage.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState, useRef } from 'react';
+import { getAirports } from '../utilities/connection';
+import '../styles/Homepage.css';
+import Selection from '../components/Selection';
+import Results from '../components/Results';
+
+const Homepage = () => {
+  const [data, setData] = useState({
+    airports: [],
+    input: {
+      vehicle: '',
+      distance: 0,
+      from: '',
+      to: '',
+      travelers: 0,
+    },
+    flight: {
+      to: {
+        journey: ['ABC', 'DEF'],
+        miles: [],
+      },
+      from: {
+        journey: [],
+        miles: [],
+      },
+    },
+    constructed: {
+      travel: {
+        selected: {
+          vehicle: '',
+          cost: 0,
+        },
+        suggested: {
+          vehicle: '',
+          cost: 0,
+        },
+      },
+      to: {
+        journey: [],
+      },
+      from: {
+        journey: [],
+      },
+    },
+  });
+
+  useEffect(() => {
+    const makeAsync = async () => {
+      const { data } = await getAirports();
+      setData((prev) => {
+        return { ...prev, airports: data };
+      });
+    };
+    makeAsync();
+  }, []);
+
+  useEffect(() => {
+    scrollToRef.current.scrollIntoView({ behavior: 'smooth' });
+  }, [data.input]);
+
+  const scrollToRef = useRef();
+
+  return (
+    <div className="Homepage">
+      <Selection data={data} setData={setData} />
+      <Results data={data} scrollToRef={scrollToRef} />
+    </div>
+  );
+};
+
+export default Homepage;

ff824ac7e1353e52c0a5cdb31bbd8e55bd8fac5c
Gareth Dawson <gwdawson.work@gmail.com>
2022-07-14 19:04:31 +0100

    feat: render flight data results

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,28 +1,68 @@
 import React from 'react';
+import { BiErrorCircle } from 'react-icons/bi';
 import ReactTooltip from 'react-tooltip';
 import '../styles/Homepage.css';
 import Ticket from '../components/Ticket';
 
-const Results = ({ data, scrollToRef }) => {
+const Results = ({ data }) => {
   const { constructed } = data;
-  const { to, from } = constructed;
+  const { to } = constructed;
   const { journey: journeyTo } = to;
-  const { journey: journeyFrom } = from;
+
+  const selectedVehicle = data.constructed.travel.selected.vehicle;
+  const selectedVehicleCost = data.constructed.travel.selected.cost;
+  const suggestedVehicle = data.constructed.travel.suggested.vehicle;
+  const suggestedVehicleCost = data.constructed.travel.suggested.cost;
+  const travelToCost = data.constructed.to.cost;
+  const travelFromCost = data.constructed.from.cost;
 
   return (
     <>
-      <div ref={scrollToRef} className="results">
-        {journeyTo.length > 0 && (
-          <>
-            <Ticket
-              first={data.flight.to.journey[0]}
-              last={data.flight.to.journey[data.flight.to.journey.length - 1]}
-              layovers={data.flight.to.journey.length - 2}
-              outbound={true}
-            />
-            <div className="data">data here...</div>
-          </>
-        )}
+      <div className="results" style={{ display: journeyTo.length > 0 ? '' : 'none' }}>
+        <>
+          <Ticket
+            first={data.flight.to.journey[0]}
+            last={data.flight.to.journey[data.flight.to.journey.length - 1]}
+            layovers={data.flight.to.journey.length - 2}
+            outbound={true}
+          />
+          <div className="data">
+            <div className="data-wrapper">
+              <h3>{data.constructed.to.journey.join(' ')}</h3>
+              {selectedVehicle !== suggestedVehicle && <ReactTooltip place="top" type="dark" effect="solid" />}
+              <h4
+                style={{ cursor: selectedVehicle !== suggestedVehicle ? 'pointer' : 'text' }}
+                data-tip={`traveling by ${suggestedVehicle.toLowerCase()} will only cost ${suggestedVehicleCost}`}
+              >
+                {selectedVehicle !== suggestedVehicle && <BiErrorCircle className="icon" />}
+                Transfer ({`${selectedVehicle.slice(0).toLowerCase()}`}): {selectedVehicleCost}
+              </h4>
+              <h4>Flight: {travelToCost}</h4>
+            </div>
+          </div>
+        </>
+      </div>
+      <hr />
+      <div className="results" style={{ display: journeyTo.length > 0 ? '' : 'none' }}>
+        <>
+          <Ticket
+            first={data.flight.from.journey[0]}
+            last={data.flight.from.journey[data.flight.from.journey.length - 1]}
+            layovers={data.flight.from.journey.length - 2}
+            outbound={false}
+          />
+          <div className="data">
+            <div className="data-wrapper">
+              <h3>{data.constructed.from.journey.join(' ')}</h3>
+              <h4>Flight: {travelFromCost}</h4>
+            </div>
+          </div>
+        </>
+      </div>
+      <div className="results" style={{ display: journeyTo.length > 0 ? '' : 'none' }}>
+        <h1>
+          Total Travel Costs: {selectedVehicleCost} + {travelToCost} + {travelFromCost}
+        </h1>
       </div>
     </>
   );
